3c6151cce5a31b3d996c9953c9da482d56231e39
metanoia1989 <sogaxili@gmail.com>
2020-07-08 23:08:33 +0800

    添加单分支if语句

--- CoreProgramming/chapter02_condition/system_performance_monitoring.sh
+++ CoreProgramming/chapter02_condition/system_performance_monitoring.sh
@@ -31,3 +31,66 @@ CPU处于IO等待的时间，用户态系统消耗的CPU统计数据
 vmstat 命令输出的前2行是头部信息，第3行为开机到当前的平均数据，
 第4行开始的数据是每秒实时数据，tail -n +4 表示去掉前三行从第四行开始显示
 EOF
+# irq 中断
+# cs 上下文切换
+# usertime 用户态CPU
+# system 系统态CPU
+# iowait 等待IO时间
+irq=$(vmstat 1 2 | tail -n +4 | tr -s" " | cut -d" " -f12)
+cs=$(vmstat 1 2 | tail -n +4 | tr -s" " | cut -d" " -f13)
+usertime=$(vmstat 1 2 | tail -n +4 | tr -s" " | cut -d" " -f14)
+systime=$(vmstat 1 2 | tail -n +4 | tr -s" " | cut -d" " -f15)
+iowait=$(vmstat 1 2 | tail -n +4 | tr -s" " | cut -d" " -f17)
+
+echo "$irq $cs $usertime $systime $iowait"
+
+# 当剩余内存不足1G时发送邮件给root报警
+[ $free_mem -lt 1048576 ] && echo "$local_time Free memory not enough.
+Free_mem: $free_mem on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当CPU的15分钟负载均衡超过4时 发送邮件给root报警
+result=$(echo "$cpu_load > 4" | bc)
+[ $result -eq 1 ] && echo "$local_time CPU load to high 
+CPU 15 averageload: $cpu_load on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当系统实时在线人数超过3人时发送邮件给root报警
+[ $login_user -gt 3 ] && echo "$local_time Too many user.
+$login_user users login to $local_ip" | \
+mail -s Warning root@localhost
+
+# 当实时进程数量大于500时发送邮件给root报警
+[ $procs -gt 500 ] && echo "$local_time Too many procs.
+$procs proc are runing on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当实时CPU中断数量大于5000时发送邮件给root报警
+[ $irq -gt 5000 ] && \
+echo "$local_time Too many interupts.
+There are $irq interupts on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当实时CPU上下文切换数量大于5000时发送邮件给root报警
+[ $cs -gt 5000 ] && \
+echo "$local_time Too many Content Switches.
+$cs of context switches per second on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当用户态进程占用超70%时发送邮件给root报警
+[ $username -gt 70 ] && \
+echo "$local_time CPU load to high.
+Time spend running non-kernel code: $usertime on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当内核态进程占用CPU超70%时发送邮件给root报警
+[ $systime -gt 70 ] && \
+echo "$local_time CPU load to high.
+Time spend running non-kernel code: $systime on $local_ip" | \
+mail -s Warning root@localhost
+
+# 当CPU消耗大量时间等待磁盘IO时发送邮件给root报警
+[ $iowait -gt 40 ] && \
+echo "$local_time Disk to slow.
+CPU spend too many time wait disk I/O: $iowait on $local_ip" | \
+mail -s Warning root@localhost
